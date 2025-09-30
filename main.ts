1c61fe5a90f60d7f78dfee5a66fa4c1462103117
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-09-30 13:24:11 -0400

    Simulates Cookie Clicker

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
  * Created by: Brendan O'Rourke
  * Created on: Sep 2025
@@ -10,7 +10,10 @@ let numberOfCookies = 0
 // setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+//shows the amount of cookies 
 input.onButtonPressed(Button.A, function () {
      numberOfCookies = numberOfCookies + 1
+basic.clearScreen()
      basic.showNumber(numberOfCookies)
 })
